b6bba1a2f2566acfc119a58bc359bb3ba9c84a2c
LAPTOP-UJFDFIK2\Gean <geanlucas98@hotmail.com>
2018-03-09 21:11:42 -0300

    Aula 4 - Lista com rolagem ok

--- MeusFavoritos/app/src/main/java/br/grupointegrado/tads/meusfavoritos/MainActivity.kt
+++ MeusFavoritos/app/src/main/java/br/grupointegrado/tads/meusfavoritos/MainActivity.kt
@@ -2,11 +2,16 @@ package br.grupointegrado.tads.meusfavoritos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (favorito in Favoritos.getLista()){
+            lista_favoritos.append("$favorito \n\n\n");
+        }
     }
 }
